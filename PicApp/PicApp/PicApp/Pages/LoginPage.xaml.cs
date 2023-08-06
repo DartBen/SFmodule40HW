6de3a279f7fdaad9308a09a54b2fb8ff6a89de5b
DartBen <ryabchikk@mail.ru>
2023-08-06 11:40:17 +0300

    loginPage

--- PicApp/PicApp/PicApp/Pages/LoginPage.xaml.cs
+++ PicApp/PicApp/PicApp/Pages/LoginPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,49 @@ namespace PicApp.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        private string _pwd;
         public LoginPage()
         {
             InitializeComponent();
+            _pwd = Preferences.Get("Pwd", String.Empty);
+            if (_pwd != string.Empty)
+            {
+                lblPwd.Text = "Введите пин-код для входа:";
+            }
+        }
+
+
+        private void endPwdButton_Click(object sender, EventArgs e)
+        {
+            string enterPwd = Password.Text;
+            if (_pwd == string.Empty)
+            {
+                Preferences.Set("Pwd", enterPwd);
+            }
+            else
+            {
+                if (_pwd != Password.Text)
+                {
+                    lblInfoMsg.Text = "Неверный ПИН-код";
+                    return;
+                }
+            }
+
+            Navigation.PushAsync(new DummyPage());
+        }
+
+        private void Password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Password.Text.Length != 4)
+            {
+                lblInfoMsg.Text = "ПИН-код должен состоять из 4 символов";
+                endPwdButton.IsEnabled = false;
+            }
+            else
+            {
+                endPwdButton.IsEnabled = true;
+                lblInfoMsg.Text = string.Empty;
+            }
         }
     }
 }
